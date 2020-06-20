28d3303cadfa78934a392cd6501f2c68a9f22312
maxiaoyu <maxiaoyu@qqtkj.com>
2020-06-20 11:18:52 +0800

    - 数据类型 - 6.20 ：6月19日学习内容：数据类型和数据检测

---
+++ 视频练习/数据类型 - 6.20.py
@@ -0,0 +1,16 @@
+# int () 整数
+a = 1.11
+print(int(a))  # ---> 1
+
+# float () 浮点数
+print(float(a))  # ---> 1.11
+
+# str ()
+print(str(a))  # ---> "1.11"
+
+# 数据检测
+print(type(a))   # ---> float
+
+# isinstance(待检测的值, 可能的数据类型)
+print(isinstance(a, str))  # ---> false
+
